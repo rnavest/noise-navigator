3acccf2cb9fa54a56ddd8dcbcb631e88134f624a
rnavest <59924376+rnavest@users.noreply.github.com>
2020-01-17 08:25:17 +0100

    Update demo_noise_navigator.m

--- demo_noise_navigator.m
+++ demo_noise_navigator.m
@@ -4,7 +4,11 @@ clc
 
 %% Load MRI data
 % with profiles in chronological acquisition order
-load('data.mat'); % dimensions [ks, profile, channel]
+for c = 1:8
+    load(sprintf('data_ch%d.mat', c) ); % dimensions [ks, profile, 1]
+    data(:, :, c) = data_ch; % dimensions [ks, profile, channel]
+    clear data_ch
+end
 
 %% Load required parameter values
 % required values are repetition time (TR), echo time (TE) and acquisition
@@ -14,3 +18,35 @@ load('label.mat');
 %% Calculate thermal noise (co)variance
 MR_FOV = 720; % [mm]; this is a body scan
 [t, noiseVar, noiseCovar, nNoiseSamp] = noiseCoVar_git(data, label, MR_FOV);
+
+%% Normalize noise variance
+% divide by median per channel
+noiseVar = bsxfun(@rdivide, noiseVar, median(noiseVar) );
+% remove mean per channel
+noiseVar = bsxfun(@minus, noiseVar, mean(noiseVar) );
+
+%% Calculate principal components
+[~, ~, V] = svd(noiseVar, 0);
+pc_full = noiseVar * V;
+clear V
+
+%% Select the principal component within the breathing frequency range
+% Calculate frequency spectrum
+[ft, f] = fourierCoeff(pc_full, 1 / (t(2) - t(1) ) );
+% relative respiratory power (between 0.05 and 0.8 Hz) within full frequency
+pow_rel = sum(ft(f > 0.05 & f < 0.8, :) .^ 2) ./ sum(ft .^2 );
+clear ft f
+% select principal component
+pc = pc_full(:, pow_rel == max(pow_rel) );
+clear pc_full pow_rel
+
+%% Apply Kalman filter
+[kal, omega, der, covMat] = Kalman_IEEE_legacy(pc, t, nNoiseSamp);
+
+%% Apply moving average filter
+tFilt = 1.2; % filter window length [s]
+type = 'same'; %'same' length or 'valid' without zero padding at the edges
+[tMA, ma] = MovingAverage_legacy(pc, t, tFilt, type);
+
+%% Show results
+figure, plot(t, pc, 'b', t, kal, 'g', tMA, ma, 'r')
